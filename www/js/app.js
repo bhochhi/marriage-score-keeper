bc803677e21370146f33aecb5a6fcd22b06c718a
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2014-12-24 07:36:12 -0600

    added rules template

--- www/js/app.js
+++ www/js/app.js
@@ -1,51 +1,104 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
-
+angular.module('score-keeper', ['ionic'])
+    .constant('_', window._)
     .run(function ($ionicPlatform) {
         $ionicPlatform.ready(function () {
-            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-            // for form inputs)
-            if (window.cordova && window.cordova.plugins.Keyboard) {
-                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-            }
             if (window.StatusBar) {
-                // org.apache.cordova.statusbar required
                 StatusBar.styleDefault();
             }
         });
     })
-
     .config(function ($stateProvider, $urlRouterProvider) {
-
-        // Ionic uses AngularUI Router which uses the concept of states
-        // Learn more here: https://github.com/angular-ui/ui-router
-        // Set up the various states which the app can be in.
-        // Each state's controller can be found in controllers.js
         $stateProvider
-            // setup an abstract state for the tabs directive
-            .state('marriage', {
-                url: "/marriage",
-                templateUrl: "templates/marriage.html",
-                controller: 'MarriageCtrl'
+            .state('app', {
+                url: "/app",
+                abstract: true,
+                templateUrl: "menu.html",
+                controller: "AppCtrl"
+            })
+            .state('app.players', {
+                url: "/players",
+                views: {
+                    'menuContent': {
+                        templateUrl: "players.html",
+                        controller: "PlayersCtrl"
+                    }
+                }
+            })
+            .state('app.rules', {
+                url: "/rules",
+                views: {
+                    'menuContent': {
+                        templateUrl: "rules.html",
+                        controller:"RulesCtrl"
+                    }
+                }
             });
-            //.state('marriage.players', {
-            //    url: '/players',
-            //    templateUrl: 'templates/players.html',
-            //    controller: 'PlayersCtrl'
-            //})
-            //.state('marriage.gameOn', {
-            //    url: '/gameOn',
-            //    templateUrl: 'templates/players-gameOn.html',
-            //    controller: 'GameOnCtrl'
-            //});
 
-        $urlRouterProvider.otherwise('/marriage');
 
-    });
+        $urlRouterProvider.otherwise('/app/players');
+    })
+    .factory('Players', function () {
+        var players = [{
+            name: 'Rupesh'
+        }, {
+            name: 'Samir'
+        }, {
+            name: 'Amir'
+        }
+        ];
 
+        return {
+            all: function () {
+                return players;
+            },
+            addPlayer: function (player) {
+                players.push(player);
+            },
+            removePlayer: function (index) {
+                players.splice(index, 1);
+            }
+
+        }
+    })
+    .service('SideMenu',function($ionicSideMenuDelegate){
+        this.toggleSideMenu = function () {
+            $ionicSideMenuDelegate.toggleLeft();
+        };
+    })
+    .controller('AppCtrl', function ($scope) {
+
+    })
+    .controller('PlayersCtrl', function ($scope, Players,SideMenu) {
+        $scope.players = Players.all();
+        $scope.toggleSideMenu = SideMenu.toggleSideMenu;
+
+
+
+
+        $scope.removePlayer = function (index) {
+            Players.removePlayer(index);
+        };
+
+        $scope.addPlayer = function () {
+            if ($scope.showDelete) {
+                $scope.showDelete = !$scope.showDelete;
+            }
+            Players.addPlayer({
+                name: ''
+            });
+        };
+    })
+    .factory('Rules',function(){
+        return {
+            central:true,
+            perPoint:10,
+            lostWithoutShow:10,
+            lostWithShow:3,
+            murder:false
+        }
+    })
+    .controller('RulesCtrl',function($scope,Rules,SideMenu){
+        $scope.rules = Rules;
+        $scope.toggleSideMenu = SideMenu.toggleSideMenu;
+    })
+;
